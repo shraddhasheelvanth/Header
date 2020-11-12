87f0e1af9656aea8a6d32a32859cd8321a82b445
shraddhasheelvanth <71065517+shraddhasheelvanth@users.noreply.github.com>
2020-11-12 21:16:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+jQuery(function(){
+    jQuery('#showall').hover(function(){
+          jQuery('.sol-payments').show();
+          jQuery('.content').hide();
+          jQuery('.sol-commerce').hide();
+
+   });
+   // jQuery('.showSingle').click(function(){
+   //       jQuery('.targetDiv').hide();
+   //       jQuery('#div'+$(this).attr('target')).show();
+   // });
+   jQuery('.Payment-div3-div1').click(function(){
+    jQuery('.sol-payments').hide();
+    jQuery('.content').show();
+    
+});
+ 
+});
+
+
+// jQuery(function(){
+ 
+//    jQuery('.Payment-div3-div1').click(function(){
+//     jQuery('.sol-payments').hide();
+//     jQuery('.content').show();
+    
+// });
+// });
+
+jQuery(function(){
+    jQuery('#showall-commerce').hover(function(){
+          jQuery('.sol-commerce').show();
+          jQuery('.content').hide();
+          jQuery('.sol-payments').hide();
+
+   });
+   // jQuery('.showSingle').click(function(){
+   //       jQuery('.targetDiv').hide();
+   //       jQuery('#div'+$(this).attr('target')).show();
+   // });
+   jQuery('.Commerce-div3-div1').click(function(){
+    jQuery('.sol-commerce').hide();
+    jQuery('.content').show();
+    
+});
+ 
+});
+
+jQuery(function(){
+    jQuery('#showallResources').hover(function(){
+          jQuery('.res-payments').show();
+          jQuery('.content').hide();
+        //   jQuery('.sol-payments').hide();
+
+   });
+   // jQuery('.showSingle').click(function(){
+   //       jQuery('.targetDiv').hide();
+   //       jQuery('#div'+$(this).attr('target')).show();
+   // });
+//    jQuery('.Commerce-div3-div1').click(function(){
+//     jQuery('.sol-commerce').hide();
+    // jQuery('.content').show();
+    
+});
+ 
+
+
+
+
+
+
+
+
+
+
