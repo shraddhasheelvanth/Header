a8d5bdf3c72e25004062fe2e18b282f838dab354
shraddhasheelvanth <71065517+shraddhasheelvanth@users.noreply.github.com>
2020-11-17 17:43:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 jQuery(function(){
-    jQuery('#showall').hover(function(){
+    jQuery('#showall').click(function(){
           jQuery('.sol-payments').show();
           jQuery('.content').hide();
           jQuery('.sol-commerce').hide();
@@ -28,7 +28,7 @@ jQuery(function(){
 // });
 
 jQuery(function(){
-    jQuery('#showall-commerce').hover(function(){
+    jQuery('#showall-commerce').click(function(){
           jQuery('.sol-commerce').show();
           jQuery('.content').hide();
           jQuery('.sol-payments').hide();
@@ -47,7 +47,7 @@ jQuery(function(){
 });
 
 jQuery(function(){
-    jQuery('#showallResources').hover(function(){
+    jQuery('#showallResources').click(function(){
           jQuery('.res-payments').show();
           jQuery('.content').hide();
         //   jQuery('.sol-payments').hide();
@@ -63,18 +63,133 @@ jQuery(function(){
     
 });
   
-function responsive_menu()
-{
-    var e=document.getElementById('menu');
-    if(e.className === 'solution')
-    {
-       e. className += "responsive";
-    }
-    else{
-        e.className='solution';
-    }
+// function responsive_menu()
+// {
+//     var e=document.getElementById('menu');
+//     if(e.className === 'dropdown')
+//     {
+//        e. className += "responsive";
+//     }
+//     else{
+//         e.className='dropdown';
+//     }
 
-    }
+//     }
+jQuery(function(){
+    jQuery('#iconbar').click(function(){
+          jQuery('.solution').show();
+        //   jQuery('.content').hide();
+        //   jQuery('.sol-payments').hide();
+
+   });
+});
+
+if($(window).width() < 768)
+{
+    jQuery(function(){
+        jQuery('#first').click(function(){
+            jQuery('.drop').show();
+            
+            jQuery('#second').hide();
+            jQuery('#third').hide();
+            jQuery('#fourth').hide();
+            jQuery('#fifth').hide();  
+       });
+    });
+}
+else{
+    jQuery('#first').show();
+    jQuery('#second').show();
+            jQuery('#third').show();
+            jQuery('#fourth').show();
+            jQuery('#fifth').show(); 
+}
+
+if($(window).width() < 768)
+{
+    jQuery(function(){
+        jQuery('#second').click(function(){
+            jQuery('.drop2').show();
+            
+            jQuery('#first').hide();
+            jQuery('#third').hide();
+            jQuery('#fourth').hide();
+            jQuery('#fifth').hide();  
+       });
+    });
+}
+else{
+    jQuery('#first').show();
+    jQuery('#second').show();
+            jQuery('#third').show();
+            jQuery('#fourth').show();
+            jQuery('#fifth').show(); 
+}
+
+if($(window).width() < 768)
+{
+    jQuery(function(){
+        jQuery('#third').click(function(){
+            jQuery('.drop3').show();
+            
+            jQuery('#first').hide();
+            jQuery('#second').hide();
+            jQuery('#fourth').hide();
+            jQuery('#fifth').hide();  
+       });
+    });
+}
+else{
+    jQuery('#first').show();
+    jQuery('#second').show();
+            jQuery('#third').show();
+            jQuery('#fourth').show();
+            jQuery('#fifth').show(); 
+}
+
+
+jQuery(function(){
+    jQuery('#iconbar').click(function(){
+          jQuery('.solution').show();
+        //   jQuery('.content').hide();
+        //   jQuery('.sol-payments').hide();
+      
+        jQuery('#iconbar').hide();
+        // jQuery('#close').show();
+
+        // function myFunction(x) {
+        //     x.classList.toggle("#close");
+        //   }
+
+   });
+});
+
+
+if($(window).width() < 768)
+{
+    jQuery(function(){
+        jQuery('#showall').click(function(){
+           
+            jQuery('.sol-payments').hide();
+            jQuery('#showall-overview').hide();
+            jQuery('#showall-accepatance').hide();
+            jQuery('#showall-fraud').hide();
+            jQuery('#showall-pci').hide(); 
+            jQuery('#showall-commerce').hide();
+            jQuery('#showall-card').hide();
+            jQuery('#showall-enterprise').hide(); 
+            jQuery('#showall-debit').hide();
+            jQuery('#showall-collabaration').hide();
+            jQuery('#showall-tech').hide();
+            
+
+       });
+    });
+}
+
+
+
+
 
 
 
